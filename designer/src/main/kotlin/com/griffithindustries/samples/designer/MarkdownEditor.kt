ee81a3313274f132ba2a99ab1ad8ba8d477d9639
Paul Griffith <pgriffith@ia.io>
2022-08-01 16:39:36 -0700

    Minor optimization to use builtin class

--- designer/src/main/kotlin/com/griffithindustries/samples/designer/MarkdownEditor.kt
+++ designer/src/main/kotlin/com/griffithindustries/samples/designer/MarkdownEditor.kt
@@ -1,26 +1,33 @@
 package com.griffithindustries.samples.designer
 
-import com.griffithindustries.samples.common.*
-import com.inductiveautomation.ignition.common.project.resource.*
-import com.inductiveautomation.ignition.designer.tabbedworkspace.*
-import net.miginfocom.swing.*
-import org.fife.ui.rsyntaxtextarea.*
-import org.fife.ui.rtextarea.*
-import javax.swing.event.DocumentEvent
-import javax.swing.event.DocumentListener
-import javax.swing.text.Document
+import com.griffithindustries.samples.common.MarkdownResource
+import com.inductiveautomation.ignition.client.util.gui.CatchAllListener
+import com.inductiveautomation.ignition.common.project.resource.ProjectResource
+import com.inductiveautomation.ignition.common.project.resource.ProjectResourceBuilder
+import com.inductiveautomation.ignition.common.project.resource.ResourcePath
+import com.inductiveautomation.ignition.designer.tabbedworkspace.ResourceEditor
+import com.inductiveautomation.ignition.designer.tabbedworkspace.TabbedResourceWorkspace
+import net.miginfocom.swing.MigLayout
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants
+import org.fife.ui.rtextarea.RTextScrollPane
 
-class MarkdownEditor(workspace: TabbedResourceWorkspace, path: ResourcePath) : ResourceEditor<MarkdownResource>(workspace, path) {
+class MarkdownEditor(
+    workspace: TabbedResourceWorkspace,
+    path: ResourcePath
+) : ResourceEditor<MarkdownResource>(workspace, path) {
     private lateinit var textArea: RSyntaxTextArea
 
     override fun init(resource: MarkdownResource) {
         removeAll()
         layout = MigLayout("ins 0, fill")
 
-        textArea = RSyntaxTextArea(resource.note).also {
-            it.syntaxEditingStyle = SyntaxConstants.SYNTAX_STYLE_NONE
-            add(RTextScrollPane(it), "grow")
-            it.document.addDocumentListener { event: DocumentEvent -> commit() }
+        textArea = RSyntaxTextArea(resource.note).apply {
+            syntaxEditingStyle = SyntaxConstants.SYNTAX_STYLE_NONE
+            add(RTextScrollPane(this), "grow")
+            document.addDocumentListener(object : CatchAllListener() {
+                override fun onChange() = commit()
+            })
         }
     }
 
@@ -38,21 +45,4 @@ class MarkdownEditor(workspace: TabbedResourceWorkspace, path: ResourcePath) : R
     override fun serializeResource(builder: ProjectResourceBuilder, resource: MarkdownResource) {
         builder.putData(MarkdownResource.DATA_KEY, resource.note.toByteArray())
     }
-}
-
-private fun Document.addDocumentListener(function: (DocumentEvent) -> Unit) {
-    this.addDocumentListener(object : DocumentListener {
-        override fun insertUpdate(e: DocumentEvent) {
-            function(e)
-        }
-
-        override fun removeUpdate(e: DocumentEvent) {
-            function(e)
-        }
-
-        override fun changedUpdate(e: DocumentEvent) {
-            function(e)
-        }
-
-    })
 }
