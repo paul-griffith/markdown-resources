f4cca6b7b976d5ce57f292a24d00be604e03a628
Ben <ben.d.musson@gmail.com>
2022-08-01 18:41:55 -0400

    Added automatic document commits

--- designer/src/main/kotlin/com/griffithindustries/samples/designer/MarkdownEditor.kt
+++ designer/src/main/kotlin/com/griffithindustries/samples/designer/MarkdownEditor.kt
@@ -6,6 +6,9 @@ import com.inductiveautomation.ignition.designer.tabbedworkspace.*
 import net.miginfocom.swing.*
 import org.fife.ui.rsyntaxtextarea.*
 import org.fife.ui.rtextarea.*
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
+import javax.swing.text.Document
 
 class MarkdownEditor(workspace: TabbedResourceWorkspace, path: ResourcePath) : ResourceEditor<MarkdownResource>(workspace, path) {
     private lateinit var textArea: RSyntaxTextArea
@@ -17,6 +20,7 @@ class MarkdownEditor(workspace: TabbedResourceWorkspace, path: ResourcePath) : R
         textArea = RSyntaxTextArea(resource.note).also {
             it.syntaxEditingStyle = SyntaxConstants.SYNTAX_STYLE_NONE
             add(RTextScrollPane(it), "grow")
+            it.document.addDocumentListener { event: DocumentEvent -> commit() }
         }
     }
 
@@ -34,4 +38,21 @@ class MarkdownEditor(workspace: TabbedResourceWorkspace, path: ResourcePath) : R
     override fun serializeResource(builder: ProjectResourceBuilder, resource: MarkdownResource) {
         builder.putData(MarkdownResource.DATA_KEY, resource.note.toByteArray())
     }
+}
+
+private fun Document.addDocumentListener(function: (DocumentEvent) -> Unit) {
+    this.addDocumentListener(object : DocumentListener {
+        override fun insertUpdate(e: DocumentEvent) {
+            function(e)
+        }
+
+        override fun removeUpdate(e: DocumentEvent) {
+            function(e)
+        }
+
+        override fun changedUpdate(e: DocumentEvent) {
+            function(e)
+        }
+
+    })
 }
